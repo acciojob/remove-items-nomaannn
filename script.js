4bc2066df38ed2c09f6f04b6bbb57c52187620ce
Nomaan Ahmad <94966523+nomaannn@users.noreply.github.com>
2023-04-30 08:59:37 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 //your JS code here. If required.
+function rm() {
+	let colorSelect = document.getElementById('colorSelect')
+	 // Get the selected option from the dropdown list
+  const selectedOption = colorSelect.options[colorSelect.selectedIndex];
+  
+  // Remove the selected option from the dropdown list
+  colorSelect.removeChild(selectedOption);
+}
